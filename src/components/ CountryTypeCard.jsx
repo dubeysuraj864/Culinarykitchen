9b057cea0b20e0b40772674f0d49903e94252414
Suraj Dubey <dubeysuraj864@gmail.com>
2023-01-21 17:30:14 +0530

    ui updated

--- src/components/ CountryTypeCard.jsx
+++ src/components/ CountryTypeCard.jsx
@@ -1,7 +1,7 @@
 function CountryTypeCard(props) {
   return (
     <>
-      <div className="country-type-card mx-auto w-[90vw] lg:w-[29vw] my-6 lg:mx-2 flex flex-col rounded-lg border lg:bg-transparent p-10 items-start">
+      <div className="country-type-card mx-auto w-[90vw] lg:h-[280px] lg:w-[29vw] my-6 lg:mx-2 flex flex-col justify-center rounded-lg border lg:bg-transparent p-10 items-start">
         <div className="title text-md font-bold py-4 " >{props.title}</div>
         <div className="description text-xs ">{props.description}</div>
       </div>
